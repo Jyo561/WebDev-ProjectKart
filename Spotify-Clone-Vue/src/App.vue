0769a799108a2d96a71126951f65f7609fb72bd7
Jyo561 <jyotiradityakuanar34@gmail.com>
2022-04-17 02:15:05 +0530

    Added

---
+++ Spotify-Clone-Vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+ <div class="App">
+   <LeftMenu />
+   <MainContainer />
+   <RightMenu />
+
+   <div class="background">
+     
+   </div>
+ </div>
+</template>
+
+<script>
+import './App.css'
+import LeftMenu from "./components/LeftMenu.vue";
+import MainContainer from "./components/MainContainer.vue";
+import RightMenu from "./components/RightMenu.vue";
+
+export default {
+  name: 'App',
+  components: {
+    LeftMenu,
+    MainContainer,
+    RightMenu
+  }
+}
+</script>
+
+<style>
+.app {
+  
+  color: #333;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  
+}
+</style>
